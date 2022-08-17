efe75dc9e6d86312f3d3b681aaa20282c4465d13
rene bakx <r.bakx@fontys.nl>
2022-08-17 17:08:57 +0200

    Added support for three ultrasonic sensors and follow mode

--- Code/LilyGO ESP32/MiniBot/src/main.cpp
+++ Code/LilyGO ESP32/MiniBot/src/main.cpp
@@ -4,18 +4,24 @@
 // board.
 // The MiniBot can be 3D printed and is easy to assemble.
 // For driving two servos are used, type TS90D Mini Servo - 1.6kg - Continuous.
-// It drives forward and makes a turn when an obstacle is detected using an ultrasonic sensor.
 // In addition a robot gripper mounted on the front can be controlled with two additonal servos,
 // type TS90M Mini Servo - 1.6kg (metal gearing and no continuous rotation).
-// It can also be remote controlled via Bluetooth, using the 'Arduino Bluetooth Controller' app, see
-// https://play.google.com/store/apps/details?id=com.appsvalley.bluetooth.arduinocontroller.
-//
+// Three ultrasonic sensors type HC-SR04 are supported which makes following an object possible.
+// It has the following modes:
+// MODE_RESET:
+//     The MiniBot stands still waiting for another MODE, initiated via Bluetooth or by pressing a button.
+// MODE_MANUAL:
+//     The MiniBot can be controlled manually via Bluetooth using the 'Arduino Bluetooth Controller' app, see
+//     https://play.google.com/store/apps/details?id=com.appsvalley.bluetooth.arduinocontroller.
+// MODE_AUTO:
+//     The MiniBot drives and avoids obstacles using one ultrasonic sensor.
+// MODE_FOLLOW:
+//     An object can be followed using three ultrasonic sensors.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <NewPing.h>
 #include "BluetoothSerial.h"
 // For TFT display of the the 'LilyGO TTGO T-Display V1.1 ESP32 - with 1.14 inch TFT Display' to work, in the '.pio\libdeps\ttgo-t1\TFT_eSPI\User_Setup_Select.h':
 // - Comment out the '//#include <User_Setup.h>'.
@@ -37,16 +43,21 @@ const int SERVO_GRIPPER_CLOSED = 30;
 const int SERVO_GRIPPER_OPEN_CLOSE_DELTA = 1;
 const int SERVO_GRIPPER_UP_DOWN_DELTA = 1;
 
-const int ULTRASONIC_TRIG_PIN = 2;
-const int ULTRASONIC_ECHO_PIN = 15;
-const int ULTRASONIC_MAX_DISTANCE = 100;
-const int ULTRASONIC_MAX_JUMP = 20;
+const int ULTRASONIC_TRIG_PIN = 2; // All ultrasonic sensors triggered at once.
+const int ULTRASONIC_0_ECHO_PIN = 15;
+const int ULTRASONIC_1_ECHO_PIN = 13;
+const int ULTRASONIC_2_ECHO_PIN = 12;
+const int ULTRASONIC_MAX_DISTANCE = 60;
+const int ULTRASONIC_MAX_JUMP = 10;
+const int ULTRASONIC_DELAY_MS = 100;
+const int NOF_ULTRASONIC_SENSORS = 3;
 
-const int ALLOWED_DISTANCE = 20;
+const int ALLOWED_DISTANCE = 30;
 
 const int MODE_RESET = 0;
-const int MODE_AUTO = 1;
-const int MODE_MANUAL = 2;
+const int MODE_MANUAL = 1;
+const int MODE_AUTO = 2;
+const int MODE_FOLLOW = 3;
 
 Servo servoWheelLeft;
 Servo servoWheelRight;
@@ -54,10 +65,53 @@ Servo servoGripperUpDown;
 Servo servoGripperOpenClose;
 
 TFT_eSPI tft = TFT_eSPI(135, 240);
-NewPing ping(ULTRASONIC_TRIG_PIN, ULTRASONIC_ECHO_PIN, ULTRASONIC_MAX_DISTANCE); // NewPing setup of pin and maximum distance.
 BluetoothSerial SerialBT;
 
 unsigned long backgroundColor = TFT_BLUE;
+
+volatile int distanceMicroSeconds[NOF_ULTRASONIC_SENSORS] = {0, 0, 0};
+
+void IsrEchoLeft()
+{
+  static unsigned long startTimeMicroSeconds;
+  int level = digitalRead(ULTRASONIC_0_ECHO_PIN);
+  if (level == HIGH)
+  {
+    startTimeMicroSeconds = micros();
+  }
+  else
+  {
+    distanceMicroSeconds[0] = micros() - startTimeMicroSeconds;
+  }
+}
+
+void IsrEchoMiddle()
+{
+  static unsigned long startTimeMicroSeconds;
+  int level = digitalRead(ULTRASONIC_1_ECHO_PIN);
+  if (level == HIGH)
+  {
+    startTimeMicroSeconds = micros();
+  }
+  else
+  {
+    distanceMicroSeconds[1] = micros() - startTimeMicroSeconds;
+  }
+}
+
+void IsrEchoRight()
+{
+  static unsigned long startTimeMicroSeconds;
+  int level = digitalRead(ULTRASONIC_2_ECHO_PIN);
+  if (level == HIGH)
+  {
+    startTimeMicroSeconds = micros();
+  }
+  else
+  {
+    distanceMicroSeconds[2] = micros() - startTimeMicroSeconds;
+  }
+}
 
 void setup()
 {
@@ -67,6 +121,13 @@ void setup()
 
   pinMode(BUTTON_LEFT_PIN, INPUT_PULLUP);
   pinMode(BUTTON_RIGHT_PIN, INPUT_PULLUP);
+  pinMode(ULTRASONIC_TRIG_PIN, OUTPUT);
+  pinMode(ULTRASONIC_0_ECHO_PIN, INPUT);
+  pinMode(ULTRASONIC_1_ECHO_PIN, INPUT);
+  pinMode(ULTRASONIC_2_ECHO_PIN, INPUT);
+  attachInterrupt(ULTRASONIC_0_ECHO_PIN, IsrEchoLeft, CHANGE);
+  attachInterrupt(ULTRASONIC_1_ECHO_PIN, IsrEchoMiddle, CHANGE);
+  attachInterrupt(ULTRASONIC_2_ECHO_PIN, IsrEchoRight, CHANGE);
 
   SerialBT.begin();
 
@@ -82,7 +143,7 @@ void setup()
   tft.setCursor(0, 0, 2);
   // Set the font colour to be white with a black background, set text size multiplier to 1
   tft.setTextColor(TFT_WHITE, backgroundColor);
-  tft.setTextSize(3);
+  tft.setTextSize(2);
   // We can now plot text on screen using the "print" class.
   tft.println("Hello World!");
 
@@ -95,6 +156,7 @@ void setup()
 
 void loop()
 {
+  // put your main code here, to run repeatedly:
   static int mode = MODE_RESET;
   static unsigned long previousBackgroundColor = TFT_BLUE;
   static int previousButtonLeftState = HIGH;
@@ -103,35 +165,52 @@ void loop()
   static int servoGripperOpenClosePos = 90;
   static int ServoGripperUpDownDelta = 0;
   static int ServoGripperOpenCloseDelta = 0;
-  static int previousMeasuredDistance = ULTRASONIC_MAX_DISTANCE;
-  static int previousReportedDistance = ULTRASONIC_MAX_DISTANCE;
-  int measuredDistance;
-  int distance;
+  static unsigned long previousMillis = 0;
+  static int previousMeasuredDistance[NOF_ULTRASONIC_SENSORS] = {ULTRASONIC_MAX_DISTANCE, ULTRASONIC_MAX_DISTANCE, ULTRASONIC_MAX_DISTANCE};
+  static int distance[NOF_ULTRASONIC_SENSORS] = {ULTRASONIC_MAX_DISTANCE, ULTRASONIC_MAX_DISTANCE, ULTRASONIC_MAX_DISTANCE};
+  int measuredDistance[NOF_ULTRASONIC_SENSORS];
 
-  // put your main code here, to run repeatedly:
-  measuredDistance = ping.ping_cm(ULTRASONIC_MAX_DISTANCE);
+  // Read out ultrasonic sensors and trigger for the next read.
+  // Multiple ultrasonic sensors are used which widens the FOV and makes it possible to approach a target or to follow an object.
+  // The ultrasonic sensors are all triggered using the same trigger signal. This to have consistent measurements during motion.
+  // Reading the length of the echo pulse is done using an interrupt for each sensor.
+  // The results is accurate distance measurements and no use of a delay (except for the short 10 µs trigger pulse). This in contrary to for example the NewPing library, which blocks waiting for the end of the echo pulse.
+  //
   // According to the datasheet https://www.elecrow.com/download/HC_SR04%20Datasheet.pdf, he HC-SR04 ultrasonic sensor normally should have an echo pulse timeout of 38 ms if no object is in sight.
   // This means the echo signal will last maximum appr. 38 ms which corresponds to appr 13 meter back and forth or 6.5 meter distance.
   // However, not all HC-SR04 clones have this timeout. The echo pulse can last > 200 ms if no object is in sight and right after that give a false short echo pulse (< 1 ms) when triggered.
   // The result is false short distance readings when no object is in sight.
   // Therefore single distance measurement jumps > ULTRASONIC_MAX_JUMP are filtered out.
-  // The NewPing library returns 0 (NO_ECHO) when the distance > ULTRASONIC_MAX_DISTANCE.
-  if (measuredDistance == 0)
-  {
-    measuredDistance = ULTRASONIC_MAX_DISTANCE;
-  }
-  if (abs(measuredDistance - previousMeasuredDistance) > ULTRASONIC_MAX_JUMP)
-  {
-    distance = previousReportedDistance;
-  }
-  else
-  {
-    distance = measuredDistance;
-  }
-  previousMeasuredDistance = measuredDistance;
-  previousReportedDistance = distance;
 
-  backgroundColor = distance < ALLOWED_DISTANCE ? TFT_RED : TFT_BLUE;
+  // Wait for ULTRASONIC_DELAY_MS before triggering the ultrasonic sensors to prevent false echoes.
+  if (millis() - previousMillis > ULTRASONIC_DELAY_MS)
+  {
+    for (int i = 0; i < NOF_ULTRASONIC_SENSORS; i++)
+    {
+      if (distanceMicroSeconds[i] != 0)
+      {
+        measuredDistance[i] = distanceMicroSeconds[i] / 58;
+
+        if (measuredDistance[i] > ULTRASONIC_MAX_DISTANCE)
+        {
+          measuredDistance[i] = ULTRASONIC_MAX_DISTANCE;
+        }
+        if (abs(measuredDistance[i] - previousMeasuredDistance[i]) <= ULTRASONIC_MAX_JUMP)
+        {
+          distance[i] = measuredDistance[i];
+        }
+        previousMeasuredDistance[i] = measuredDistance[i];
+        distanceMicroSeconds[i] = 0; // Indicate that this measurement has been handled. It will be set again by the ISR at the next echo pulse.
+      }
+    }
+    // Trigger the ultrasonic sensors for the next measurement.
+    digitalWrite(ULTRASONIC_TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(ULTRASONIC_TRIG_PIN, LOW);
+    previousMillis = millis();
+  }
+
+  backgroundColor = distance[1] < ALLOWED_DISTANCE ? TFT_RED : TFT_BLUE;
 
   // Only set background color when it changes to prevent flicker.
   if (backgroundColor != previousBackgroundColor)
@@ -141,16 +220,31 @@ void loop()
     previousBackgroundColor = backgroundColor;
   }
   tft.setCursor(0, 0, 2);
-  tft.println("Dist: \n" + String(distance) + " cm          ");
+  tft.println("Distances:\n" + String(distance[0]) + " cm          ");
+  tft.setCursor(0, tft.fontHeight() * 2, 2);
+  tft.println(String(distance[1]) + " cm          ");
+  tft.setCursor(0, tft.fontHeight() * 3, 2);
+  tft.println(String(distance[2]) + " cm");
 
-  // Switch between automatic mode and reset mode as soon as one of the onboard buttons is pressed.
+  // Switch between modes when one of the onboard buttons is pressed.
   int buttonLeftState = digitalRead(BUTTON_LEFT_PIN);
   int buttonRightState = digitalRead(BUTTON_RIGHT_PIN);
-  if (previousButtonLeftState == HIGH && previousButtonRightState == HIGH && (buttonLeftState == LOW || buttonRightState == LOW))
+  if (previousButtonLeftState == HIGH && buttonLeftState == LOW)
   {
     if (mode == MODE_RESET)
     {
       mode = MODE_AUTO;
+    }
+    else
+    {
+      mode = MODE_RESET;
+    }
+  }
+  if (previousButtonRightState == HIGH && buttonRightState == LOW)
+  {
+    if (mode == MODE_RESET)
+    {
+      mode = MODE_FOLLOW;
     }
     else
     {
@@ -166,13 +260,13 @@ void loop()
     // switch to manual mode as soon Bluetooth command comes in.
     mode = MODE_MANUAL;
     cmd = SerialBT.read();
-    tft.setCursor(0, 100, 2);
+    tft.setCursor(0, tft.fontHeight() * 4, 2);
     tft.println("Cmd: \n" + String(cmd));
   }
 
   if (mode == MODE_AUTO)
   {
-    if (distance < ALLOWED_DISTANCE)
+    if (distance[1] < ALLOWED_DISTANCE)
     {
       // Turn left or right randomly.
       if (random(2) == 0)
@@ -189,6 +283,40 @@ void loop()
     }
     else
     {
+      servoWheelLeft.write(180);
+      servoWheelRight.write(0);
+    }
+  }
+  else if (mode == MODE_FOLLOW)
+  {
+    if (distance[1] < ALLOWED_DISTANCE)
+    {
+      // Stop.
+      servoWheelLeft.write(90);
+      servoWheelRight.write(90);
+    }
+    else if (distance[1] <= distance[0] && distance[1] <= distance[2])
+    {
+      // Straight forward.
+      servoWheelLeft.write(180);
+      servoWheelRight.write(0);
+    }
+
+    else if (distance[0] < distance[2])
+    {
+      // Turn left.
+      servoWheelLeft.write(90);
+      servoWheelRight.write(70);
+    }
+    else if (distance[0] > distance[2])
+    {
+      // Turn right.
+      servoWheelLeft.write(110);
+      servoWheelRight.write(90);
+    }
+    else
+    {
+      // Straight forward.
       servoWheelLeft.write(180);
       servoWheelRight.write(0);
     }
@@ -225,9 +353,13 @@ void loop()
     case '4':
       ServoGripperOpenCloseDelta = -SERVO_GRIPPER_OPEN_CLOSE_DELTA;
       break;
-    case 'a':
+    case 'A':
       // switch back to automatic mode.
       mode = MODE_AUTO;
+      break;
+    case 'F':
+      // switch back to target mode.
+      mode = MODE_FOLLOW;
       break;
     case '0':
       // Forward, backward, left, write button release, stop motion.
